18fb055318bdce3cc1afa2922972a55a61117170
Utkarshmishra67 <kartiksun716@gmail.com>
2026-04-02 11:43:01 +0530

    Utkarsh: updateprofile.html

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('update-profile/', views.update_profile, name='update_profile'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def dashboard(request):
      return render(request, 'dashboard/dashboard.html')
-  
+def update_profile(request):
+    return render(request, 'dashboard/updateprofile.html')
+
+@login_required
+def dashboard(request):
+    profile = request.user.profile   # 🔥 get logged-in user data
+
+    return render(request, 'dashboard/dashboard.html', {
+        'profile': profile
+    })
