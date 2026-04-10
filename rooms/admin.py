96e3447fbe8289c87fa19def89c57eb8802832df
Manish kumar <mkbbu91101@gmail.com>
2026-04-10 19:51:03 +0530

    add admin .py on rooms app

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Amenity, Listing, ListingImage
 
-# Register your models here.
+# Inhe register karne se ye Admin Panel mein dikhne lagenge
+admin.site.register(Amenity)
+admin.site.register(Listing)
+admin.site.register(ListingImage)
