72e6c0a8020956a3d084825c98a0e18ea75f6207
Manish kumar <mkbbu91101@gmail.com>
2026-04-13 14:31:15 +0530

    dashboard bottom html updated

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from rooms.models import Listing
+from inbox.views import get_conversations  
 
-
+#==========profile===============
 def get_user_profile_or_none(user):
     """Return user profile if available, else None."""
     try:
         return user.profile
     except Exception:
         return None
+    
+    
+
+#==========dashboard view============
 
 
 @login_required(login_url='login')
 def dashboard(request):
-    # Login user ka profile (agar ho)
     user_profile = get_user_profile_or_none(request.user)
-
-    # Latest published listings show karne ke liye
     listings = Listing.objects.filter(is_published=True).prefetch_related('images').order_by('-created_at')[:6]
 
-    context = {'profile': user_profile, 'listings': listings}
+    
+    recent_messages = get_conversations(request.user)[:5]
+
+    context = {
+        'profile': user_profile,
+        'listings': listings,
+        'recent_messages': recent_messages,  
+    }
     return render(request, 'dashboard/dashboard.html', context)
